02c0ab152c9cae764f71f5f0341eaa850d21a4e7
Hakan-kuru <hakan.k3272@gmail.com>
2025-01-05 19:53:32 +0300

    Bi'şeyler deniyoruz
    finaller yaklaştı github'a ara verme vakti geldi

--- app/src/main/java/com/example/Travelling_App/data/database/local/entity/UserEntity.kt
+++ app/src/main/java/com/example/Travelling_App/data/database/local/entity/UserEntity.kt
@@ -1,14 +1,19 @@
 package com.example.Travelling_App.data.database.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 import androidx.room.Entity
 
 @Entity(tableName = "user_table")
+//@Entity içinde (tableName = "user_table" ) kullanılmasaydı tablo ismi otomatik olarak clas ismi-User- olurdu
 data class UserEntity(
-    @PrimaryKey val userId: Int,
-    val username: String,
-    val email: String,
-    val budget: Double?,
-    val preferences: String?,
-    val password: String
+    //@PrimaryKey(autoGenerate) benzersiz bir id oluşturur
+    @PrimaryKey(autoGenerate = true) val userId: Int,
+    //ColumnInfo(name = "name") burada kolon ismi belirtilir,
+    // eğer kullanılmasaydı column ismi otomatik olarak değişken ismi-username- olurdu
+    @ColumnInfo(name = "name") val username: String,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "budget") val budget: Double?,
+    @ColumnInfo(name = "preferences") val preferences: String?,
+    @ColumnInfo(name = "password") val password: String
 )
